2fe953d61cf0b400c6a53a089955c81add0f970e
abhirami09 <55319187+abhirami09@users.noreply.github.com>
2020-08-03 18:22:47 +0530

    method added by developer2

--- src/main/java/com/abhi/GitDemo/App.java
+++ src/main/java/com/abhi/GitDemo/App.java
@@ -12,6 +12,12 @@ public class App
 			System.out.println("Welcome");
 		}
 	}
+	
+	public void addedMethod(){
+		System.out.println("new method");
+		
+	}
+		
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
